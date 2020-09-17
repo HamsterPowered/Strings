a411f04735e5e784215dfc0298626138f68d670b
Gabriel Karpinsky <gabriel.karpinsky@gmail.com>
2020-09-17 20:34:49 +0200

    Initial commit

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Strings
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Give name");
+        String name=sc.nextLine();
+        System.out.println(name);
+    }
+
+}
